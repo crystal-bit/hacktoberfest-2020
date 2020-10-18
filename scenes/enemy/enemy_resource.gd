8adc5315b013f80122e7edfb52caf5a8f5003cd7
mRuggi <kirito.poppox@gmail.com>
2020-10-18 12:33:41 +0200

    update resources

--- scenes/enemy/enemy_resource.gd
+++ scenes/enemy/enemy_resource.gd
@@ -13,8 +13,31 @@ func get_name():
 		EnemyTypes.ENEMY_TYPES.COMMANDO:
 			return "Commando"
 		EnemyTypes.ENEMY_TYPES.ARMORED_TRANSPORT:
-			return "Armoted Transport"
+			return "Armored Transport"
 		EnemyTypes.ENEMY_TYPES.TANK:
 			return "Tank"
 		EnemyTypes.ENEMY_TYPES.AIRCRAFT:
 			return "Aircraft"
+
+func get_hp():
+	match type:
+		EnemyTypes.ENEMY_TYPES.SOLDIER:
+			if(EnemyTypes.GRADE==NORMAL): 
+				return 30
+			else return 60
+		EnemyTypes.ENEMY_TYPES.COMMANDO:
+			if(EnemyTypes.GRADE==NORMAL): 
+				return 80
+			else return 120
+		EnemyTypes.ENEMY_TYPES.ARMORED_TRANSPORT:
+			if(EnemyTypes.GRADE==NORMAL): 
+				return 70
+			else return	100
+		EnemyTypes.ENEMY_TYPES.TANK:
+			if(EnemyTypes.GRADE==NORMAL): 
+				return	250
+			else return	750
+		EnemyTypes.ENEMY_TYPES.AIRCRAFT:
+			if(EnemyTypes.GRADE==NORMAL): 
+				return	150
+			else return	300
